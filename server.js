c6191244677deefebd0d094c3d96a80c36a68849
qais-alsgher <qaisalsgher@gmail.com>
2022-08-12 21:32:32 +0300

    database mongo atlas and CRUD method

---
+++ server.js
@@ -0,0 +1,106 @@
+'use strict';
+require('dotenv').config();
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+// test connect
+app.get('/test', (request, response) => {
+
+    response.send('test is ok :)')
+
+})
+
+// CRUD method
+app.get('/news', readNews);
+app.post('/news', addNews);
+app.delete('/news/:_id', deleteNews);
+app.put('/news/:_id', updatNews);
+
+
+// read data from database by get method
+function readNews(req, res) {
+
+    newsModel.find({}, (error, data) => {
+        if (error) console.log(`read from database have error:${error}`)
+        else res.send(data);
+    })
+}
+
+// add new news by post method 
+
+function addNews(req, res) {
+    const { newsAdd } = req.body;
+    const news = new newsModel(newsAdd);
+    news.save();
+    res.status(201).json(news);
+}
+
+// delete news by delete method
+
+function deleteNews(req, res) {
+    const newsId = req.params._id;
+    newsModel.findByIdAndDelete(newsId).then(record => {
+        res.send(record)
+    }).catch(error => {
+        res.status(500).send(error.message);
+    })
+}
+
+// update data  news bt put method
+function updatNews(req, res) {
+    const newsId = req.params._id;
+    const { updata } = req.body;
+    newsModel.findByIdAndUpdate(newsId, updata, { new: true }).then(record => {
+        res.send(record);
+    }).catch(error => {
+        res.status(500).send(error.message);
+    })
+}
+
+
+
+
+// connect mongoose atlas
+mongoose.connect('mongodb+srv://qais-alsgher:qais123@cluster0.evdbsxk.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useUnifiedTopology: true
+    });
+
+const newsSchema = new mongoose.Schema({
+
+    auther: String,
+    title: String,
+    description: String,
+    url: String,
+    image: String,
+    category: String,
+    published_at: String
+
+})
+
+const newsModel = mongoose.model('newModel', newsSchema);
+
+
+const news1 = newsModel({
+    auther: "qais alsgher",
+    title: "qais alsgher",
+    description: "qais alsgher",
+    url: "qais alsgher",
+    image: "qais alsgher",
+    category: "qais alsgher",
+    published_at: "qais alsgher"
+})
+
+// news1.save();
+
+
+
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
