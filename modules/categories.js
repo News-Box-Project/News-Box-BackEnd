8047ef0cfe0f02f6117f5b96be841d496453b2cd
Wlla Talafha <wlla.issa396@gmail.com>
2022-08-12 20:20:40 +0300

    add the cache memory

--- modules/categories.js
+++ modules/categories.js
@@ -1,18 +1,25 @@
 "use strict";
 
 const axios = require("axios");
-// const { News } = require("./News");
+const categCache={};
 
 async function handleCategory(req, res) {
   const categories = req.query.categories;
   const categUrl = `http://api.mediastack.com/v1/news?access_key=${process.env.API_KEY}&languages=en&categories=${categories}`;
-  try {
-    const axiosData = await axios.get(categUrl);
-    const categData = axiosData.data.data.map((item) => new News(item));
-    res.status(200).send(categData);
-  } catch (error) {
-    res.status(422).send(error.message);
+
+  if (categCache[categories] !== undefined){
+    res.status(200).send(categCache[categories]);
+  }else {
+    try {
+      const axiosData = await axios.get(categUrl);
+      const categData = axiosData.data.data.map((item) => new News(item));
+      categCache[categories]=categData;    
+      res.status(200).send(categData);
+    } catch (error) {
+      res.status(422).send(error.message);
+    }
   }
+
 }
 
 class News {
