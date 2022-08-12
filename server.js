70603c9dc7cfc85bdd69284e8fcf4a4c80aee074
Wlla Talafha <wlla.issa396@gmail.com>
2022-08-12 17:30:41 +0300

    get data from 3rd party API

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const axios =require('axios');
 
 const server = express();
 server.use(cors());
@@ -9,15 +10,46 @@ server.use(cors());
 const PORT = process.env.PORT || 3001;
 
 //End points
+//http://api.mediastack.com/v1/news?access_key=10396027d7341f9122e9dcfbc14078de&keywords=keywords&languages=en
+server.get("/news",handleSearch);
 server.get("/test", handleTest);
+
 
 //Handler Functions
 function handleTest(req, res) {
   res.send("Test Test");
 }
+async function handleSearch ( req, res) {
+  const keywords=req.query.keywords;
+  const newsUrl=`http://api.mediastack.com/v1/news?access_key=${process.env.API_KEY}&keywords=${keywords}&languages=en`;
+  try{
+  const axiosData=await axios.get(newsUrl);
+  const newsData=axiosData.data.data.map(item=>new News(item));
+  res.status(200).send(newsData);
+  console.log(newsData);
+ 
+}
+catch(error){
+  res.status(500).send("there is no data");
+}
+  
+  
+  }
 
 server.get("*", (req, res) => {
-  res.status(500).send("No Request");
+  res.status(404).send("Page not found !");
 });
 
 server.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+class News {
+  constructor(obj){
+    this.author=obj.author;
+    this.title=obj.title;
+    this.description=obj.description;
+    this.url=obj.url;
+    this.image=obj.image;
+    this.category=obj.category;
+    this.published_at=obj.published_at;
+  }
+}
